cc24f79d03c90293e6230f57913541609279e118
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-22 18:40:31 +0000

    connect to database

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+// Define database connection constants
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'MKTIMEportfolio');
+
+// Attempt to connect  to MySQL database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection, otherwise exit and display error message
+if (!$link) {
+    die('Could not connect to MySQL: ' . mysqli_error());
+}
+
+?>
+
+<!---->
