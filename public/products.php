63b40c59627553df4e21194c02c64379bb78db94
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-23 15:28:28 +0000

    update products to use mysql table

--- public/products.php
+++ public/products.php
@@ -1,6 +1,7 @@
 <?php
 include 'session.php';
 include '../templates/nav.php';
+require '../config/database.php';
 ?>
 
 <!doctype html>
@@ -30,57 +31,48 @@ include '../templates/nav.php';
       <div class="col-lg-8">
 
         <div class="card-group">
-          <div class="card text-center m-3">
-            <img src="assets/images/watch1.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Fabulous Watch</h5>
-              <p class="card-text">Text placeholder for description of this really fabulous, lovely watch.</p>
-            </div>
-            <div class="card-footer bg-light">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">42 MM STEEL ON STEEL</li>
-                <li class="list-group-item">£3,000</li>
-                <li class="list-group-item">
-                  <a href="#" class="btn btn-primary">Add to basket</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <?php
+          // Query to select all records from the products table
+          $query = "SELECT * FROM products";
+          $result = mysqli_query($link, $query);
 
-          <div class="card text-center m-3">
-            <img src="assets/images/watch2.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Very Very Fabulous Watch</h5>
-              <p class="card-text">Text placeholder for really long text description of this really fabulous, lovely watch. Text is longer but the card height is the same.</p>
-            </div>
-            <div class="card-footer bg-light">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">42 MM STEEL ON STEEL</li>
-                <li class="list-group-item">£3,000</li>
-                <li class="list-group-item">
-                  <a href="#" class="btn btn-primary">Add to basket</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="card text-center m-3">
-            <img src="assets/images/watch3.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Very Very Fabulous Watch</h5>
-              <p class="card-text">Text placeholder for really long text description of this really fabulous, lovely watch. Text is longer but the card height is the same.</p>
-            </div>
-            <div class="card-footer bg-light">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">42 MM STEEL ON STEEL</li>
-                <li class="list-group-item">£3,000</li>
-                <li class="list-group-item">
-                  <a href="#" class="btn btn-primary">Add to basket</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
+          if (mysqli_num_rows($result) > 0) {
+            echo '<div class="container"><div class="row">';
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+              echo '
+              <div class="col-md-4">
+                <div class="card text-center m-3">
+                  <img src="../' . htmlspecialchars($row['item_img']) . '" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">' . htmlspecialchars($row['item_name']) . '</h5>
+                    <p class="card-text">' . htmlspecialchars($row['item_desc']) . '</p>
+                  </div>
+                  <div class="card-footer bg-light">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">' . htmlspecialchars($row['item_price']) . '</li>
+                      <li class="list-group-item">
+                        <a href="#" class="btn btn-primary">Add to basket</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>';
+            }
+            echo '</div></div>';
+            // Close database connection.
+            mysqli_close($link);
+          } else {
+            echo '
+            <div class="col-md-4">
+              <div class="card text-center m-3">
+              <div class="card-body">
+                <h5 class="card-title">No Products Available</h5>
+                <p class="card-text">There are currently no items in the table to display.</p>
+              </div>
+              </div>
+            </div>';
+          }
+          ?>
         </div>
       </div>
     </div>
