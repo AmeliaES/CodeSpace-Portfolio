43f1db2a71ab8d6b550d74bf92348676853500b2
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-23 11:49:38 +0000

    user_id not definied in sql table

--- sql/make_users.sql
+++ sql/make_users.sql
@@ -5,14 +5,16 @@
 -- country, password
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) NOT NULL,
     phone_number VARCHAR(20),
     address_line_1 VARCHAR(255) NOT NULL,
     address_line_2 VARCHAR(255),
     country VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    reg_date DATETIME NOT NULL
+    reg_date DATETIME NOT NULL,
+    PRIMARY KEY (user_id),
+    UNIQUE (email)
 );
