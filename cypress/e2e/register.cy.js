bb6cbb1ca0a321b54e9c11e1da6b3532b1c5bd81
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-29 16:24:08 +0000

    clean up test data before each test

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -3,21 +3,12 @@ describe('registration page creates new user', () => {
   const email = 'test+test@MKTIMEdomainForTesting.com';
   const deleteUserQuery = `mysql -u root -S /Applications/XAMPP/xamppfiles/var/mysql/mysql.sock -D MKTIMEportfolio -e "DELETE FROM users WHERE email='${email}'"`;
 
-  before(() => {
+  beforeEach(() => {
     // Remove test user before tests start
     cy.exec(deleteUserQuery).then((result) => {
       cy.log('User cleanup before test:', result);
     });
-  });
 
-  after(() => {
-    // Remove test user after tests complete
-    cy.exec(deleteUserQuery).then((result) => {
-      cy.log('User cleanup after test:', result);
-    });
-  });
-
-  beforeEach(() => {
     cy.visit('http://localhost/codespace/MKTIME/public/reg.php');
   });
 
@@ -53,6 +44,19 @@ describe('registration page creates new user', () => {
   });
 
   it('checks for duplicate email error', () => {
+    // Create a user
+    cy.get('[data-cy="firstName"]').type('TestUserForCypress');
+    cy.get('[data-cy="lastName"]').type('TestUserForCypressLastName');
+    cy.get('[data-cy="email"]').type('test+test@MKTIMEdomainForTesting.com');
+    cy.get('[data-cy="phone"]').type('1234567890');
+    cy.get('[data-cy="adLine1"]').type('123 Test Address');
+    cy.get('[data-cy="adLine2"]').type('Test Address Line 2');
+    cy.get('[data-cy="country"]').type('UK');
+    cy.get('[data-cy="password"]').type('123456');
+    cy.get('[data-cy="confirmPassword"]').type('123456');
+    cy.get('[data-cy="submit"]').click();
+
+    // Create a duplicate user
     cy.get('[data-cy="firstName"]').type('TestUserForCypress');
     cy.get('[data-cy="lastName"]').type('TestUserForCypressLastName');
     cy.get('[data-cy="email"]').type('test+test@MKTIMEdomainForTesting.com');
@@ -70,7 +74,21 @@ describe('registration page creates new user', () => {
     );
   });
 
-  it('checks for empty fields error', () => {});
-
-  it('checks for password mismatch error', () => {});
+  it('checks for password mismatch error', () => {
+    cy.get('[data-cy="firstName"]').type('TestUserForCypress');
+    cy.get('[data-cy="lastName"]').type('TestUserForCypressLastName');
+    cy.get('[data-cy="email"]').type('test+test@MKTIMEdomainForTesting.com');
+    cy.get('[data-cy="phone"]').type('1234567890');
+    cy.get('[data-cy="adLine1"]').type('123 Test Address');
+    cy.get('[data-cy="adLine2"]').type('Test Address Line 2');
+    cy.get('[data-cy="country"]').type('UK');
+    cy.get('[data-cy="password"]').type('123456');
+    cy.get('[data-cy="confirmPassword"]').type('qwertyasdfg');
+    cy.get('[data-cy="submit"]').click();
+    // Check toast for error
+    cy.get('[data-cy="toastText"]').should(
+      'include.text',
+      'Passwords do not match'
+    );
+  });
 });
