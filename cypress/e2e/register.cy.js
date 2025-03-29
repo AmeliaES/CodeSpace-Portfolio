64694af02198a3b4f3fd44779771eabe99f39bdd
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-29 16:11:14 +0000

    clean up test user from database

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,4 +1,16 @@
 describe('registration page creates new user', () => {
+  // Clean up the database and remove test user
+  before(() => {
+    // Define the SQL query to delete a user by email
+    const email = 'test+test@MKTIMEdomainForTesting.com';
+    const query = `mysql -u root -S /Applications/XAMPP/xamppfiles/var/mysql/mysql.sock -D MKTIMEportfolio -e "DELETE FROM users WHERE email='${email}'"`;
+
+    // Execute the SQL query via exec()
+    cy.exec(query).then((result) => {
+      cy.log(result); // Log the result of the query execution
+    });
+  });
+
   beforeEach(() => {
     cy.visit('http://localhost/codespace/MKTIME/public/reg.php');
   });
@@ -19,7 +31,7 @@ describe('registration page creates new user', () => {
   it('registers a new user succesfully', () => {
     cy.get('[data-cy="firstName"]').type('TestUserForCypress');
     cy.get('[data-cy="lastName"]').type('TestUserForCypressLastName');
-    cy.get('[data-cy="email"]').type('test_user123@email.com');
+    cy.get('[data-cy="email"]').type('test+test@MKTIMEdomainForTesting.com');
     cy.get('[data-cy="phone"]').type('1234567890');
     cy.get('[data-cy="adLine1"]').type('123 Test Address');
     cy.get('[data-cy="adLine2"]').type('Test Address Line 2');
@@ -29,8 +41,30 @@ describe('registration page creates new user', () => {
     cy.get('[data-cy="submit"]').click();
     // get toast and check for success message
     cy.get('[data-cy="toastText"]').should(
-      'have.text',
+      'include.text',
       'You are now registered'
     );
   });
+
+  it('checks for duplicate email error', () => {
+    cy.get('[data-cy="firstName"]').type('TestUserForCypress');
+    cy.get('[data-cy="lastName"]').type('TestUserForCypressLastName');
+    cy.get('[data-cy="email"]').type('test+test@MKTIMEdomainForTesting.com');
+    cy.get('[data-cy="phone"]').type('1234567890');
+    cy.get('[data-cy="adLine1"]').type('123 Test Address');
+    cy.get('[data-cy="adLine2"]').type('Test Address Line 2');
+    cy.get('[data-cy="country"]').type('UK');
+    cy.get('[data-cy="password"]').type('123456');
+    cy.get('[data-cy="confirmPassword"]').type('123456');
+    cy.get('[data-cy="submit"]').click();
+    // get toast and check for error message
+    cy.get('[data-cy="toastText"]').should(
+      'include.text',
+      'Email address already registered.'
+    );
+  });
+
+  it('checks for empty fields error', () => {});
+
+  it('checks for password mismatch error', () => {});
 });
