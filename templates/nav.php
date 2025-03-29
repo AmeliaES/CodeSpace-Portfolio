b410e8b1fb66b8b859384e029ad5872e88a53002
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2025-03-29 12:45:58 +0000

    add button for cart with number of items in cart

--- templates/nav.php
+++ templates/nav.php
@@ -30,6 +30,22 @@
               <a class="nav-link" href="reg.php">Register</a>
             </li>
           <?php endif; ?>
+          <!-- Show Cart icon with item count if cart has items -->
+          <?php if (!empty($_SESSION['cart'])): ?>
+            <?php
+            // Calculate total number of items in the cart
+            $cartItemCount = 0;
+            foreach ($_SESSION['cart'] as $item) {
+              $cartItemCount += $item['quantity'];
+            }
+            ?>
+            <li class="nav-item">
+              <a class="nav-link" href="cart.php">
+                <i class="bi bi-cart-fill"></i>
+                <span class="badge rounded-pill bg-light ms-1"><?php echo $cartItemCount; ?></span> <!-- Cart item count -->
+              </a>
+            </li>
+          <?php endif; ?>
         </ul>
       </span>
     </div>
